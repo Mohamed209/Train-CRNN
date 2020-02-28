b13240c19cf75fd497bf9a4c6e9a031cb8f9f184
mohamed209 <mohamedmosad209@gmail.com>
2020-02-29 00:39:53 +0200

    :tada: added generation basic configs

---
+++ generate_training_lines.py
@@ -0,0 +1,118 @@
+import arabic_reshaper
+import pyarabic.araby as araby
+import cv2
+import PIL
+import uuid
+import re
+import numpy as np
+from bidi.algorithm import get_display
+from trdg.generators import GeneratorFromStrings
+from trdg.utils import add_parallel_light
+from scipy.stats import norm
+from PIL import Image
+from data_loader import pull_wikipedia_content
+
+SHADOW_DISTRIBUTION = [1, 0]
+SHADOW_WEIGHT = [0.3, 0.7]
+INV_DISTRIBUTION = [1, 0]
+INV_WEIGHT = [0.3, 0.7]
+FIT = False
+SAVE_PATH = 'dataset/generated_data/'
+ara_lines = []
+eng_lines = []
+ara_lines_no_res = []
+mixed_lines = []
+mixed_lines_no_res = []
+text_size = [90, 100, 110]
+blur = [0, 1]
+skewing_angle = [0, 1, 2]
+background_type = [0, 1, 2]
+distorsion_type = [0, 2, 3]
+text_color = ["#000000", "#282828", "#505050"]
+
+
+def add_fake_shdows(pil_img):
+    open_cv_image = np.array(pil_img)
+    # Convert RGB to BGR
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+    open_cv_image = add_parallel_light(open_cv_image)
+    return Image.fromarray(open_cv_image)
+
+
+def invert(pil_img):
+    open_cv_image = np.array(pil_img)
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+    inv = cv2.bitwise_not(open_cv_image)
+    return Image.fromarray(inv)
+
+
+def generate_english_lines():
+    # pull_wikipedia_content(language='english')
+    with open('dataset/text_corpus/eng_gt.txt') as f:
+        for line in f.readlines():
+            if line.strip():
+                eng_lines.append(line)
+
+
+def generate_mixed_lines():
+    with open('dataset/text_corpus/db_gt.txt', mode='r', encoding='utf-8') as f:
+        for line in f.readlines():
+            if line.strip():
+                mixed_lines.append(line)
+        mixed_lines_no_res.extend(mixed_lines)
+        for i in range(len(mixed_lines)):
+            mixed_lines[i] = arabic_reshaper.reshape(mixed_lines[i])
+            mixed_lines[i] = get_display(mixed_lines[i])
+
+
+####################################################################
+'''
+create N generators and randomly select one for each iteration
+'''
+####################################################################
+
+generate_english_lines()
+generate_mixed_lines()
+english_generator = GeneratorFromStrings(
+    strings=eng_lines,
+    language='en',
+    count=1000,
+    size=np.random.choice(text_size),
+    distorsion_type=np.random.choice(distorsion_type),
+    skewing_angle=np.random.choice(skewing_angle),
+    blur=np.random.choice(blur),
+    background_type=np.random.choice(background_type),
+    text_color=np.random.choice(text_color)
+)
+mixed_generator = GeneratorFromStrings(
+    strings=mixed_lines,
+    language='mix',
+    count=1000,
+    size=np.random.choice(text_size),
+    distorsion_type=np.random.choice(distorsion_type),
+    skewing_angle=np.random.choice(skewing_angle),
+    blur=np.random.choice(blur),
+    background_type=np.random.choice(background_type),
+    text_color=np.random.choice(text_color)
+)
+print("started generating lines :)")
+for img, lbl in english_generator:
+    if np.random.choice(SHADOW_DISTRIBUTION, p=SHADOW_WEIGHT):
+        img = add_fake_shdows(img)
+    elif np.random.choice(INV_DISTRIBUTION, p=INV_WEIGHT):
+        img = invert(img)
+    img = img.resize((432, 32), Image.ANTIALIAS)
+    ID = str(uuid.uuid4())
+    img.save(SAVE_PATH+ID+'.png')
+    with open(SAVE_PATH+ID+'.txt', 'w', encoding='utf-8') as label:
+        label.writelines(lbl)
+for img, lbl in mixed_generator:
+    if np.random.choice(SHADOW_DISTRIBUTION, p=SHADOW_WEIGHT):
+        img = add_fake_shdows(img)
+    elif np.random.choice(INV_DISTRIBUTION, p=INV_WEIGHT):
+        img = invert(img)
+    img = img.resize((432, 32), Image.ANTIALIAS)
+    ID = str(uuid.uuid4())
+    img.save(SAVE_PATH+ID+'.png')
+    with open(SAVE_PATH+ID+'.txt', 'w', encoding='utf-8') as label:
+        label.writelines(mixed_lines_no_res[mixed_lines.index(lbl)])
---
+++ receipt_ocr/train_model.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from keras.callbacks import ModelCheckpoint
+from keras.utils import to_categorical
+import keras.backend as K
+from keras import optimizers
+from keras.activations import relu, sigmoid, softmax
+from keras.models import Model
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPooling2D, Lambda, Bidirectional
+from keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import os
+import pyarabic.araby as araby
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+# Check all available devices if GPU is available
+print(device_lib.list_local_devices())
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+# utils
+letters = ' '+araby.LETTERS
+
+
+def labels_to_text(labels):
+    return ''.join(list(map(lambda x: letters[int(x)], labels)))
+
+
+def text_to_labels(text):
+    return list(map(lambda x: letters.index(x), text))
+
+# # Loss and train functions, network architecture
+
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    # the 2 is critical here since the first couple outputs of the RNN
+    # tend to be garbage:
+    y_pred = y_pred[:, 2:, :]
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+
+# data loader
+img_h = 32
+img_w = 432
+downsample_factor = 4
+DATA_PATH = '../dataset/generated_data/'
+data = sorted(os.listdir())
+images = np.zeros(shape=(len(data)//2, img_h, img_w, 1))
+label_length = np.zeros((len(data)//2, 1), dtype=np.int)
+input_length = np.ones((len(data)//2, 1)) * (img_w // downsample_factor - 2)
+text = []
+i = 0
+j = 0
+for sample in data:
+    if sample.split('.')[-1] == 'png':
+        img = cv2.imread(DATA_PATH+sample, cv2.IMREAD_GRAYSCALE)
+        # img = cv2.resize(img, (img_w, img_h))
+        img = img.astype(np.float32)
+        img = (img / 255.0)
+        img = np.expand_dims(img, axis=-1)
+        images[i] = img
+        i += 1
+    else:
+        with open(DATA_PATH+sample, 'r') as s:
+            sent = s.readlines()
+            text.append(sent)
+            label_length[j] = len(sent[0])
+        j += 1
+
+
+# show sample of data
+# plt.imshow(images[6][:, :, -1])
+
+
+# we need number repr for text
+gt_text = []
+textnum = []
+for line in text:
+    data = line[0]
+    textnum.append(text_to_labels(data))
+for i in range(len(textnum)):
+    gt_text.append(textnum[i])
+gt_text
+
+
+gt_padded_txt = pad_sequences(gt_text, maxlen=40, padding='post', value=0)
+gt_padded_txt
+
+
+print("images >>", images.shape)
+print("text >>", gt_padded_txt.shape)
+print("input length >>", input_length.shape)
+print("label length>>", label_length.shape)
+
+
+# train validation split
+train_size = 0.8
+xtrain = images[: int(train_size*images.shape[0])]
+xtest = images[int(train_size*images.shape[0]):]
+ytrain = gt_padded_txt[: int(train_size*gt_padded_txt.shape[0])]
+ytest = gt_padded_txt[int(train_size*gt_padded_txt.shape[0]):]
+train_input_length = input_length[: int(train_size*input_length.shape[0])]
+test_input_length = input_length[int(train_size*input_length.shape[0]):]
+train_label_length = label_length[: int(train_size*label_length.shape[0])]
+test_label_length = label_length[int(train_size*label_length.shape[0]):]
+
+
+# network >>>>> CNN + RNN + CTC loss
+# input with shape of height=32 and width=432
+inputs = Input(shape=(32, 432, 1))
+
+# convolution layer with kernel size (3,3)
+conv_1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+# poolig layer with kernel size (2,2)
+pool_1 = MaxPooling2D(pool_size=(2, 2), strides=2)(conv_1)
+
+conv_2 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool_1)
+pool_2 = MaxPooling2D(pool_size=(2, 2), strides=2)(conv_2)
+
+conv_3 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool_2)
+
+conv_4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv_3)
+# poolig layer with kernel size (2,1)
+pool_4 = MaxPooling2D(pool_size=(2, 1))(conv_4)
+
+conv_5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool_4)
+# Batch normalization layer
+batch_norm_5 = BatchNormalization()(conv_5)
+
+conv_6 = Conv2D(512, (3, 3), activation='relu', padding='same')(batch_norm_5)
+batch_norm_6 = BatchNormalization()(conv_6)
+pool_6 = MaxPooling2D(pool_size=(2, 1))(batch_norm_6)
+
+conv_7 = Conv2D(512, (2, 2), activation='relu')(pool_6)
+# feature maps to sequence
+squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
+
+# bidirectional LSTM layers with units=256
+blstm_1 = Bidirectional(
+    LSTM(256, return_sequences=True, dropout=0.2))(squeezed)
+blstm_2 = Bidirectional(LSTM(256, return_sequences=True, dropout=0.2))(blstm_1)
+
+outputs = Dense(len(letters)+1, activation='softmax')(blstm_2)
+
+test_model = Model(inputs, outputs)
+
+print(test_model.summary())
+
+
+test_model.save('test_model.h5')
+
+max_label_len = 40
+labels = Input(name='the_labels', shape=[max_label_len], dtype='float32')
+input_length = Input(name='input_length', shape=[1], dtype='int64')
+label_length = Input(name='label_length', shape=[1], dtype='int64')
+
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    # y_pred = y_pred[:, 2:, :]
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+
+loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')(
+    [outputs, labels, input_length, label_length])
+
+# model to be used at training time
+train_model = Model(
+    inputs=[inputs, labels, input_length, label_length], outputs=loss_out)
+
+
+batch_size = 128
+epochs = 1000
+adam = optimizers.adam(lr=1e-4, momentum=0.9, decay=1e-1 / epochs)
+train_model.compile(
+    loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=adam)
+checkpoint = ModelCheckpoint(
+    filepath='ckpts/CRNN--{epoch:02d}--{val_loss:.3f}.hdf5', monitor='val_loss', verbose=1, mode='min', period=100)
+train_model.fit(x=[xtrain, ytrain, train_input_length, train_label_length],
+                y=np.zeros(ytrain.shape[0]),
+                validation_data=(
+                    [xtest, ytest, test_input_length, test_label_length], np.zeros(ytest.shape[0])),
+                callbacks=[checkpoint],
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=1)
+
+
+# predict outputs on validation images
+# test_model.load_weights('CRNN--500--147.804.hdf5')
+# test_image = images[1][:, :, -1]
+# test_image = np.expand_dims(test_image, -1)
+# test_image = np.expand_dims(test_image, axis=0)
+# prediction = test_model.predict(test_image)
+# plt.imshow(images[1][:, :, -1])
+# # use CTC decoder
+# out = K.get_value(K.ctc_decode(prediction, input_length=np.ones(prediction.shape[0])*prediction.shape[1],
+#                                greedy=True)[0][0])
+# # see the results
+# i = 0
+# for x in out:
+#     print("predicted text = ", end='')
+#     for p in x:
+#         if int(p) != -1:
+#             print(letters[int(p)], end='')
+#     print('\n')
+#     i += 1
